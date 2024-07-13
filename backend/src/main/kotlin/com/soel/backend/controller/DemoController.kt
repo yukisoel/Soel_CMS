ec341a01f9bfa176ad63fde9f8bfc75a53e41d09
TaharaKanta <piraruku66@gmail.com>
2024-07-13 19:27:30 +0900

    backend init

---
+++ backend/src/main/kotlin/com/soel/backend/controller/DemoController.kt
@@ -0,0 +1,15 @@
+package com.soel.backend.controller
+
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
+
+@RestController
+@RequestMapping("/api/demo")
+class DemoController {
+
+  @GetMapping("/hello")
+  fun hello ():String {
+    return "Hello World"
+  }
+}
