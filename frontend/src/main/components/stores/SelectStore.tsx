9dec832aae91a338b7e375debc3015e03dc1035b
harutaka814 <harutaka814@gmail.com>
2025-01-11 15:48:16 +0900

    新しいタブを追加

--- frontend/src/main/components/stores/SelectStore.tsx
+++ frontend/src/main/components/stores/SelectStore.tsx
@@ -1,13 +1,26 @@
 import styles from "@/main/components/stores/SelectStore.module.scss";
 import {GoogleService} from "@/main/service/GoogleService.ts";
 import AdvancedSidebarMenu from "@/main/common/AdvancedSidebarMenu";
+import Wrapper from "@/main/common/Wrapper";
+import Typography from "@/main/common/Typography";
+import { useAdvancedTabs } from "@/main/common/AdvancedTabs/useAdvancedTabs";
 
 type Props = {
   googleService: GoogleService
 }
 
 export default function SearchStore({googleService}: Props) {
+  const { tabsRender } = useAdvancedTabs([
+    {tabKey: 'brand', content: 'ブランドから選択'},
+    {tabKey: 'area', content: 'エリアから選択'}
+  ])
   return (
-    <AdvancedSidebarMenu />
+    <Wrapper className={styles.wrapper}>
+      <AdvancedSidebarMenu />
+      <Wrapper direction="col" gap="5rem" padding="5rem 4.3rem 5.9rem 5rem" className={styles.content_container}>
+        <Typography content="投稿する店舗を選択" color="primary" size="medium" />
+        {tabsRender()}
+      </Wrapper>
+    </Wrapper>
   )
 }
