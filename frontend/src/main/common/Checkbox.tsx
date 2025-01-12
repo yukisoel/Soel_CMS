f46323a64bb571a6cfb8a9551497fa2aa7f3a63c
harutaka814 <harutaka814@gmail.com>
2025-01-12 12:40:20 +0900

    Checkbox追加

---
+++ frontend/src/main/common/Checkbox.tsx
@@ -0,0 +1,26 @@
+import styles from "@/main/common/Checkbox.module.scss";
+import Typography from "@/main/common/Typography";
+import { useId } from "react";
+
+type Props = {
+  label?: string
+  supplementaryText?: string
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+}
+
+export default function Checkbox({label, supplementaryText, onChange}: Props) {
+  const id = useId()
+  return (
+    <>
+        <input type="checkbox" id={id} className={styles.custom_checkbox} onChange={onChange} />
+        <label htmlFor={id} className={styles.custom_checkbox_label} >
+            {label && (
+                <Typography content={label} color="primary" size="normal" />
+            )}
+            {supplementaryText && (
+                <Typography content={supplementaryText} color="secondary" size="normal" className={styles.supplementary_text} />
+            )}
+        </label>
+    </>
+  )
+}
--- frontend/src/main/components/stores/SelectStore.tsx
+++ frontend/src/main/components/stores/SelectStore.tsx
@@ -4,6 +4,8 @@ import AdvancedSidebarMenu from "@/main/common/AdvancedSidebarMenu";
 import Wrapper from "@/main/common/Wrapper";
 import Typography from "@/main/common/Typography";
 import { useAdvancedTabs } from "@/main/common/AdvancedTabs/useAdvancedTabs";
+import SearchBox from "@/main/common/SearchBox";
+import Checkbox from "@/main/common/Checkbox";
 
 type Props = {
   googleService: GoogleService
@@ -20,6 +22,13 @@ export default function SearchStore({googleService}: Props) {
       <Wrapper direction="col" gap="5rem" padding="5rem 4.3rem 5.9rem 5rem" className={styles.content_container}>
         <Typography content="投稿する店舗を選択" color="primary" size="medium" />
         {tabsRender()}
+        <SearchBox placeholder="店舗名を検索" onChange={() => {}} width="42.7rem" />
+        <Checkbox supplementaryText="全ての店舗" onChange={() => {}} />
+        <Checkbox label="鳥貴族" onChange={() => {}} />
+        <Checkbox label="鳥貴族" supplementaryText="(1000店舗)" onChange={() => {}} />
+        <div style={{width: "200px"}}>
+        <Checkbox label="文字数が多い店舗は改行したいです。" onChange={() => {}} />
+        </div>
       </Wrapper>
     </Wrapper>
   )
