c1d3277a061a53f6577ba9b522ad6695a9ea2a48
harutaka814 <harutaka814@gmail.com>
2025-01-25 16:35:43 +0900

    写真一覧画面を作成

--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -17,6 +17,7 @@ import EditReservationLayout from "@/main/components/editPage/EditReservationLay
 import EditProductLayout from "./components/editPage/EditProductLayout/EditProductLayout.tsx";
 import EditPageAdvanced from "./pages/EditPageAdvanced.tsx";
 import SchedulePost from "./components/stores/SchedulePost/SchedulePost.tsx";
+import BlukPhoto from "./components/stores/BulkPhoto/BulkPhoto.tsx";
 
 const googleRepository = new GoogleRepositoryImpl()
 const googleService = new GoogleServiceImpl({googleRepository})
@@ -74,6 +75,8 @@ function App() {
         }>
           <Route path={'bulk/schedule-post'}
             element={<SchedulePost googleService={googleService} />} />
+          <Route path={'bulk/photo'}
+            element={<BlukPhoto googleService={googleService} />} />
       </Route>
     </Routes>
   )
