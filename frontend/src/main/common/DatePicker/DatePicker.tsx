6ee7550da0f6bd723efe759afae6f747fd5c6bc7
harutaka814 <harutaka814@gmail.com>
2025-02-06 11:08:39 +0900

    DatePicker, TimePickerを実装

---
+++ frontend/src/main/common/DatePicker/DatePicker.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import ReactDatePicker, { registerLocale } from 'react-datepicker';
+import { ja } from 'date-fns/locale/ja';
+import 'react-datepicker/dist/react-datepicker.css';
+import './DatePicker.scss';
+import styles from './DatePicker.module.scss';
+import ArrowIcon from '@/main/assets/ArrowIcon.svg';
+
+registerLocale('ja', ja);
+
+type Props = {
+    defaultValue?: Date;
+    onChange?: (date: Date | null) => void;
+};
+
+const DatePicker: React.FC<Props> = ({ defaultValue, onChange }) => {
+    const [selectedDate, setSelectedDate] = useState<Date | null>(defaultValue || null);
+
+    const handleChange = (date: Date | null) => {
+        setSelectedDate(date);
+        if (onChange) {
+            onChange(date);
+        }
+    };
+
+    return (
+        <div className={styles.date_picker_container}>
+            <ReactDatePicker
+                selected={selectedDate}
+                onChange={handleChange}
+                locale="ja"
+                dateFormat="yyyy年MM月dd日"
+                placeholderText="日付を選択"
+                className={styles.date_picker_input}
+            />
+            <img src={ArrowIcon} alt="Arrow Icon" className={styles.arrow_icon} />
+        </div>
+    );
+};
+
+export default DatePicker;
---
+++ frontend/src/main/common/TimePicker/TimePicker.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import ReactDatePicker, { registerLocale } from 'react-datepicker';
+import { ja } from 'date-fns/locale/ja';
+import 'react-datepicker/dist/react-datepicker.css';
+import './TimePicker.scss'
+import styles from './TimePicker.module.scss';
+import ArrowIcon from '@/main/assets/ArrowIcon.svg';
+
+registerLocale('ja', ja);
+
+type Props = {
+    defaultValue?: Date;
+    onChange?: (date: Date | null) => void;
+};
+
+const TimePicker: React.FC<Props> = ({ defaultValue, onChange }) => {
+    const [selectedTime, setSelectedTime] = useState<Date | null>(defaultValue || null);
+
+    const handleChange = (time: Date | null) => {
+        setSelectedTime(time);
+        if (onChange) {
+            onChange(time);
+        }
+    };
+
+    return (
+        <div className={styles.time_picker_container}>
+            <ReactDatePicker
+                selected={selectedTime}
+                onChange={handleChange}
+                showTimeSelect
+                showTimeSelectOnly
+                timeIntervals={15}
+                timeCaption="時間"
+                dateFormat="HH:mm"
+                locale="ja"
+                placeholderText="00:00"
+                className={styles.time_picker_input}
+            />
+            <img src={ArrowIcon} alt="Arrow Icon" className={styles.arrow_icon} />
+        </div>
+    );
+};
+
+export default TimePicker;
