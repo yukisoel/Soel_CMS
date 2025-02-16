8e2fb6a419a78c30a20347916a5182a112a87545
harutaka814 <harutaka814@gmail.com>
2025-02-16 15:58:32 +0900

    Modalコンポーネントを作成

--- frontend/src/main/common/FileUploadModal/FileUploadModal.tsx
+++ frontend/src/main/common/FileUploadModal/FileUploadModal.tsx
@@ -1,9 +1,6 @@
 import styles from './FileUploadModal.module.scss';
 import FileUpload from '../FileUpload/FileUpload';
-import Typography from '../Typography';
-import CloseIcon from '@/main/assets/CloseIcon.svg';
-import Separator from '../Separator';
-import Wrapper from '../Wrapper';
+import Modal from '../Modal/Modal';
 
 type Props = {
     isOpen: boolean;
@@ -17,17 +14,8 @@ export default function FileUploadModal({ isOpen, onClose, size, uploadedPhotoFi
     if (!isOpen) return null;
 
     return (
-        <div className={styles.modal_overlay}>
-            <div className={styles.modal_content}>
-                <Wrapper direction="col" gap="2rem">
-                    <div className={styles.modal_header}>
-                        <Typography content="写真をアップロード" color="primary" size="normal" />
-                        <button className={styles.close_button} onClick={onClose}>
-                            <img src={CloseIcon} alt="close icon"  />
-                        </button>
-                    </div>
-                    <Separator width="100%" />
-                </Wrapper>
+        <Modal headerContent="写真をアップロード" isOpen={isOpen} onClose={onClose} contentRender={() => (
+            <>
                 <FileUpload setUploadedPhotoFileList={setUploadedPhotoFileList} size={size} />
                 {uploadedPhotoFileList && (
                     <div className={styles.uploaded_files}>
@@ -38,7 +26,6 @@ export default function FileUploadModal({ isOpen, onClose, size, uploadedPhotoFi
                         ))}
                     </div>
                 )}
-            </div>
-        </div>
-    );
-}
+            </>)}
+        />
+    )}
