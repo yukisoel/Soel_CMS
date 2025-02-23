d951cf9a2f785781b63401407bd83b4f850268bb
TaharaKanta <piraruku66@gmail.com>
2025-02-23 21:53:45 +0900

    Revert "Revert "update:menuのlogを記録する機能の追加""
    This reverts commit d48931bf33a5699f43dba9ff3c8af51af2406751.

--- backend/src/main/kotlin/com/soel/backend/backend/entity/MenuLogs.kt
+++ backend/src/main/kotlin/com/soel/backend/backend/entity/MenuLogs.kt
@@ -1,6 +1,8 @@
 package com.soel.backend.backend.entity
 
+import io.hypersistence.utils.hibernate.type.json.JsonType
 import jakarta.persistence.*
+import org.hibernate.annotations.Type
 import java.time.LocalDateTime
 
 @Entity
@@ -16,7 +18,7 @@ data class MenuLog(
     val locationId: String,
     @Column(name = "created_at")
     val createdAt: LocalDateTime = LocalDateTime.now(),
-
+    @Type(JsonType::class)
     @Column(name = "menu", columnDefinition = "jsonb")
     val menu: String
 ){
