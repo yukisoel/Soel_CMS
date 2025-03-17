ec6ac1b8031207584f5fb9da04f901bcc62fd49d
harutaka814 <harutaka814@gmail.com>
2025-03-17 11:52:17 +0900

    scroll対応

--- frontend/src/main/components/stores/SelectStore/SelectedStoreList.tsx
+++ frontend/src/main/components/stores/SelectStore/SelectedStoreList.tsx
@@ -3,6 +3,7 @@ import Wrapper from "@/main/common/Wrapper"
 import Typography from "@/main/common/Typography"
 import Button from "@/main/common/Button"
 import React from "react";
+import Scroll from "@/main/common/Scroll";
 
 type Props = {
     selectedStores: string[]
@@ -14,16 +15,18 @@ export default function SelectedStoreList({ selectedStores, onBackClick }: Props
         <Wrapper direction="col" gap="1.6rem">
             <Typography content="選択中の店舗" color="primary" size="normal" />
             <Wrapper gap="1rem" className={styles.selected_store_container}>
-                <Wrapper padding="2rem 2.5rem 2.5rem" className={styles.selected_store}>
-                    {selectedStores.map((store, index) => (
-                        <React.Fragment key={index}>
-                            <Typography content={store} color="primary" size="normal" />
-                            {index !== selectedStores.length - 1 && (
-                                <Typography content="|" color="primary" size="normal" />
-                            )}
-                        </React.Fragment>
-                    ))}
-                </Wrapper>
+                <Scroll height="150px" width="884px">
+                    <Wrapper className={styles.selected_store}>
+                        {selectedStores.map((store, index) => (
+                            <React.Fragment key={index}>
+                                <Typography content={store} color="primary" size="normal" />
+                                {index !== selectedStores.length - 1 && (
+                                    <Typography content="|" color="primary" size="normal" />
+                                )}
+                            </React.Fragment>
+                        ))}
+                    </Wrapper>
+                </Scroll>
                 <Button bgColor="primary" padding="0.7rem" className={styles.back_button} onClick={onBackClick}>
                     <Typography content="編集" color="primary" size="normal" />
                 </Button>
