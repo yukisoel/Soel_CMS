2bdbcbb7040a9acc52c930e7920eef699c22a59b
TaharaKanta <piraruku66@gmail.com>
2025-06-20 22:46:30 +0900

    googleでログインできていない時のリダイレクト先を変更

--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -32,7 +32,7 @@ function App() {
     .then(_res => {
     })
     .catch(_ => {
-      window.location.href = '/oauth2/authorization/cognito'
+      window.location.href = '/login'
     })
 
   return (
