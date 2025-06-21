658f630e0d416ece437c3919d6aea2f55f0df6ba
TaharaKanta <piraruku66@gmail.com>
2025-06-22 03:00:22 +0900

    修正

--- backend/src/main/kotlin/com/soel/backend/backend/controller/database/BrandController.kt
+++ backend/src/main/kotlin/com/soel/backend/backend/controller/database/BrandController.kt
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/brand")
-class CognitoController(val brandService: BrandService) {
+class BrandController(val brandService: BrandService) {
     @Operation(
         summary = "ユーザーが持つブランド一覧を取得",
         description = """
