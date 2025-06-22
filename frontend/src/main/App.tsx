7a4f81a089b914096d7156a2221d755c9a3a5c0b
TaharaKanta <piraruku66@gmail.com>
2025-06-22 15:51:57 +0900

    フロントのログイン処理追加

--- frontend/src/main/App.tsx
+++ frontend/src/main/App.tsx
@@ -28,21 +28,7 @@ const googleService = new GoogleServiceImpl({googleRepository})
 
 function App() {
 
-  axiosApiClient.get('google/me')
-    .then(_res => {
-    })
-    .catch(err => {
-      const reason = err.response.headers['x-auth-error']
-      if (reason === 'cognito_required') {
-        // Cognito 未ログインを検知
-        window.location.href = '/oauth2/authorization/cognito'
-      } else if (reason === 'google_required') {
-        // Google 連携未完了を検知
-        window.location.href = '/oauth2/authorization/google'
-      } else {
-        console.error(err)
-      }
-    })
+  axiosApiClient.get('cognito/me')
 
   return (
     <Routes>
