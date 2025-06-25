75ce87018de3273da2eadf40ec5c8558a8be197a
TaharaKanta <piraruku66@gmail.com>
2025-06-26 01:10:57 +0900

    tesk定義のバージョン更新

--- terraform/aws_ecs.tf
+++ terraform/aws_ecs.tf
@@ -30,6 +30,10 @@ locals {
           "valueFrom" = "${data.aws_secretsmanager_secret.project_secrets.arn}:GOOGLE_CLIENT_SECRET::"
         },
         {
+          "name"      = "GOOGLE_SCOPE"
+          "valueFrom" = "${data.aws_secretsmanager_secret.project_secrets.arn}:GOOGLE_SCOPE::"
+        },
+        {
           "name"      = "COGNITO_USER_POOL_ID"
           "valueFrom" = "${data.aws_secretsmanager_secret.project_secrets.arn}:COGNITO_USER_POOL_ID::"
         },
